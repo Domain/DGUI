4f43e2a8b40cf852091433546bacdb3356c7efe9
Dax <trogu.davide@gmail.com>
2013-09-25 21:50:03 +0200

    [License] Change license to Boost License 1.0.

--- dgui/core/events/focuseventargs.d
+++ dgui/core/events/focuseventargs.d
@@ -1,4 +1,12 @@
-﻿module dgui.core.events.focuseventargs;
+﻿/** DGui project file.
+
+Copyright: Trogu Antonio Davide 2011-2013
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Trogu Antonio Davide
+*/
+module dgui.core.events.focuseventargs;
 
 public import dgui.core.events.eventargs;
 
